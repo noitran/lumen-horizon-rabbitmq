d863527f0a8d25ea8ed44549b46abfa080896adc
F <artur@web-design.lv>
2018-10-25 14:32:44 +0300

    Initial commit

---
+++ src/Jobs/RabbitMQJob.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Iocaste\Lumen\Horizon\Jobs;
+
+use VladimirYuldashev\LaravelQueueRabbitMQ\Queue\Jobs\RabbitMQJob as BaseJob;
+
+class RabbitMQJob extends BaseJob
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function delete(): void
+    {
+        parent::delete();
+
+        $this->connection->deleteReserved($this->queue, $this);
+    }
+}
---
+++ src/RabbitMQQueue.php
@@ -0,0 +1,180 @@
+<?php
+
+namespace Iocaste\Lumen\Horizon;
+
+use Iocaste\Lumen\Horizon\Jobs\RabbitMQJob;
+use Illuminate\Contracts\Events\Dispatcher;
+use Illuminate\Support\Str;
+use Laravel\Horizon\Events\JobDeleted;
+use Laravel\Horizon\Events\JobPushed;
+use Laravel\Horizon\Events\JobReserved;
+use Laravel\Horizon\JobId;
+use Laravel\Horizon\JobPayload;
+use VladimirYuldashev\LaravelQueueRabbitMQ\Queue\RabbitMQQueue as BaseQueue;
+
+class RabbitMQQueue extends BaseQueue
+{
+    /**
+     * The job that last pushed to queue via the "push" method.
+     *
+     * @var object|string
+     */
+    protected $lastPushed;
+
+    /**
+     * Get the number of queue jobs that are ready to process.
+     *
+     * @param  string|null  $queue
+     *
+     * @return int
+     */
+    public function readyNow($queue = null)
+    {
+        return $this->size($queue);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function push($job, $data = '', $queue = null)
+    {
+        $this->lastPushed = $job;
+
+        return parent::push($job, $data, $queue);
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function pushRaw($payload, $queue = null, array $options = [])
+    {
+        $payload = (new JobPayload($payload))->prepare($this->lastPushed)->value;
+
+        return tap(parent::pushRaw($payload, $queue, $options), function () use ($queue, $payload): void {
+            $this->event($this->getQueue($queue), new JobPushed($payload));
+        });
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function later($delay, $job, $data = '', $queue = null)
+    {
+        $payload = (new JobPayload($this->createPayload($job, $data)))->prepare($job)->value;
+
+        return tap(parent::pushRaw($payload, $queue, ['delay' => $this->secondsUntil($delay)]), function () use ($payload, $queue): void {
+            $this->event($this->getQueue($queue), new JobPushed($payload));
+        });
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function popRaw($queueName = null)
+    {
+        try {
+            list($queue) = $this->declareEverything($queueName);
+
+            $consumer = $this->getContext()->createConsumer($queue);
+
+            if ($message = $consumer->receiveNoWait()) {
+                return new RabbitMQJob($this->container, $this, $consumer, $message);
+            }
+        } catch (\Exception $exception) {
+            $this->reportConnectionError('pop', $exception);
+        }
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function pop($queue = null)
+    {
+        return tap($this->popRaw($queue), function ($result) use ($queue): void {
+            if ($result) {
+                $this->event($this->getQueue($queue), new JobReserved($result->getRawBody()));
+            }
+        });
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function release($delay, $job, $data, $queue, $attempts = 0)
+    {
+        $this->lastPushed = $job;
+
+        return parent::release($delay, $job, $data, $queue, $attempts);
+    }
+
+    /**
+     * Fire the given event if a dispatcher is bound.
+     *
+     * @param  string  $queue
+     * @param  mixed  $event
+     *
+     * @return void
+     */
+    protected function event($queue, $event): void
+    {
+        if ($this->container && $this->container->bound(Dispatcher::class)) {
+            $queue = Str::replaceFirst('queues:', '', $queue);
+
+            $this->container->make(Dispatcher::class)->dispatch(
+                $event->connection($this->getConnectionName())->queue($queue)
+            );
+        }
+    }
+
+    /**
+     * Create a payload string from the given job and data.
+     *
+     * @param  string  $job
+     * @param  string  $queue
+     * @param  mixed   $data
+     *
+     * @return string
+     */
+    protected function createPayloadArray($job, $queue, $data = '')
+    {
+        return array_merge(parent::createPayloadArray($job, $queue, $data), [
+            'id' => $this->getRandomId(),
+            'attempts' => 0,
+        ]);
+    }
+
+    /**
+     * Fire the job deleted event.
+     *
+     * @param  string  $queue
+     * @param  \Iocaste\Lumen\Horizon\Jobs\RabbitMQJob  $job
+     *
+     * @return void
+     */
+    public function deleteReserved($queue, $job): void
+    {
+        $this->event($this->getQueue($queue), new JobDeleted($job, $job->getRawBody()));
+    }
+
+    /**
+     * Get the queue name.
+     *
+     * @param string|null $queue
+     *
+     * @return string
+     */
+    protected function getQueue($queue = null): string
+    {
+        return $queue ?: $this->queueName;
+    }
+
+    /**
+     * Get a random ID string.
+     *
+     * @return string
+     */
+    protected function getRandomId()
+    {
+        return JobId::generate();
+    }
+}
